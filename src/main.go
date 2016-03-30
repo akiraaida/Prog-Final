310e4b56f2173b319145e64f54270b0a104ba8d1
Akira <akira.aida@live.com>
2016-03-30 16:15:03 -0400

    Have the basic understanding of how to serve html/css

--- src/main.go
+++ src/main.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
-type MyMux struct {
-}
-
-func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		sayhelloName(w, r)
-		return
-	}
-	http.NotFound(w, r)
-	return
-}
-
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello myroute!")
-}
-
 func main() {
-	mux := &MyMux{}
-	http.ListenAndServe("www.akiraaida.me:80", mux)
+
+	http.Handle("/", http.FileServer(http.Dir("./index")))
+
+	err := http.ListenAndServe("www.akiraaida.me:80", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
