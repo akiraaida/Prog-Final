2a1853b4bbc010113e3420459b5c910f13f66777
Akira <akira.aida@live.com>
2016-03-30 17:06:08 -0400

    Have basic form submission handling

--- src/main.go
+++ src/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+func search(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "POST" {
+		r.ParseForm()
+		fmt.Println("username:", r.Form["fname"])
+		fmt.Println("password:", r.Form["lname"])
+	}
+}
+
 func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("./index")))
+	http.HandleFunc("/search", search)
 
 	err := http.ListenAndServe("www.akiraaida.me:80", nil)
 	if err != nil {
