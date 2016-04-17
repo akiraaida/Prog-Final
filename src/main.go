1efdc069a8c78880f8d0e803542657ca0a067cb7
akiraaida <akira.aida@live.com>
2016-04-17 13:25:17 -0400

    Have a sorted list of the top words

--- src/main.go
+++ src/main.go
@@ -6,9 +6,76 @@ import (
 	"log"
 	"net/http"
         "io"
+        "strings"
+        "sort"
 )
 
-func parse(data io.Reader) {
+func sortMap(w http.ResponseWriter, wordMap map[string]int){
+
+    // Create a map to hold the count, word equivalent
+    tempMap := make(map[int][]string)
+    
+    var counts []int
+
+    // Go through the word map and create the count, word
+    // equivalent        
+    for word, count := range wordMap {
+        tempMap[count] = append(tempMap[count], word)
+    }
+
+    // Go through the count, word map and append the counts
+    for count := range tempMap {
+        counts = append(counts, count)
+    }
+
+    // IntSlices attaches the methods of Interface to counts for sorting
+    // Reverse returns the reverse order of counts so that it will sort in reverse order
+    // Sort sorts counts thus it'll have counts going from high -> low
+    sort.Sort(sort.Reverse(sort.IntSlice(counts)))
+
+    // Iterate through counts, throwing away the index
+    num := 1
+    for _, count := range counts {
+        // Get the word where the count specifies it should be
+        for _, word := range tempMap[count] {
+            // Print the data in descending order
+            fmt.Fprintf(w, "%d: %s, %d\n", num, word, count) 
+
+            if num == 25 {
+                break
+            }
+            num++
+        }
+        if num == 25 {
+            break
+        }
+    }
+}
+
+func countOcc(w http.ResponseWriter, wordList []string){
+    
+    // Create a word map to count the occurences of the words
+    wordMap := make(map[string]int)
+
+    // Loop through every word and normalize them to lower case
+    // As well as removing characters that throw off the count
+    for i := 0; i < len(wordList); i++ {
+        
+        wordList[i] = strings.Replace(wordList[i], ".", "", -1)
+        wordList[i] = strings.Replace(wordList[i], ",", "", -1)
+        wordList[i] = strings.Replace(wordList[i], "(", "", -1)
+        wordList[i] = strings.Replace(wordList[i], ")", "", -1)        
+        wordList[i] = strings.Replace(wordList[i], "\"", "", -1)        
+        wordList[i] = strings.ToLower(wordList[i])
+
+        if wordList[i] != "" {
+            wordMap[wordList[i]]++
+        }
+    }
+    sortMap(w, wordMap)
+}
+
+func parse(w http.ResponseWriter, data io.Reader) {
     
     // HTML is tokenized using a tokenizer
     // on the response body that was fetched
@@ -55,7 +122,7 @@ func parse(data io.Reader) {
                     // If it is one of those and the word is not empty, then a word
                     // has been found and append it to the slice created at the start
                     for i := 0; i < len(byteData); i++ {
-                        if byteData[i] != 32 && byteData[i] != 10 && byteData[i] != 9{
+                        if byteData[i] != 32 && byteData[i] != 10 && byteData[i] != 9 {
                             word += string(byteData[i])
                         } else {
                             if word != "" {
@@ -75,10 +142,10 @@ func parse(data io.Reader) {
             }
         }
     } 
-    fmt.Println(wordList)
+    countOcc(w, wordList)
 }
 
-func retrieve(site string) {
+func retrieve(w http.ResponseWriter, site string) {
         
     // Get takes a string and returns a response and error
     // The error is nil if the response was successful
@@ -96,7 +163,7 @@ func retrieve(site string) {
     // Calls parse with the body that was extracted from the Get
     // The body is of type ReadCloser which uses the interface
     // io.Reader
-    parse(resp.Body)
+    parse(w, resp.Body)
 }
 
 func handleSubmit(w http.ResponseWriter, r *http.Request) {
@@ -106,7 +173,7 @@ func handleSubmit(w http.ResponseWriter, r *http.Request) {
     if r.Method == "POST" {
         r.ParseForm()
 	site := r.FormValue("website")
-	retrieve(site)
+	retrieve(w, site)
     }
 }
 
